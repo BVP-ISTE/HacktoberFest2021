c8a3bfbbc932f456d31ff0559b4eed90d1ad6f55
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-02 17:30:09 +0530

    fixed the issue.
    the code had an error in base of recurive function and also the number should be called after reaching the base case.

--- C++/Errors/print_numbers.cpp
+++ C++/Errors/print_numbers.cpp
@@ -1,22 +1,24 @@
 #include<iostream>
 using namespace std;
 
-//Given is the code to print numbers from 1 to n in increasing order recursively. 
+//Given is the code to print numbers from 1 to n in increasing order recursively.
 //But it contains few bugs that you need to rectify such that all the test cases pass.
 
-void print(int n){
-    if(n == 1){
-        cout << n << " ";
-    }
-    cout << n << " ";
-    print(n - 1);
+void print(int n) {
+	if (n == 1) {
+		cout << n << " ";
+		return;
+	}
+	print(n - 1);
+	cout << n << " ";
+
 }
 
 //No need to change the main function.
 
-int main(){
-    int n;
-    cin >> n;
-  
-    print(n);
+int main() {
+	int n;
+	cin >> n;
+
+	print(n);
 }
