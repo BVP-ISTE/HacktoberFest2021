79d129427263a8bd5e41d6e6edcb1e4c3660b3e9
Deepanshi Jain <jaindeepanshi04@gmail.com>
2021-10-02 21:14:04 +0530

    updated solution to #8 check_number
    Hey! I've solved this issue on #8. Please review it.

--- C++/Tasks/Check_number.cpp
+++ C++/Tasks/Check_number.cpp
@@ -4,17 +4,25 @@ using namespace std;
 //Given an array of length N and an integer x, you need to find if x is present in the array or not. 
 //Return true or false.  Do this recursively.
 
-bool checkNumber(int input[], int size, int x) {
+bool checkNumber(int arr[], int size, int x) {
   /* Don't write main().
      Don't read input, it is passed as function argument.
      Return output and don't print it.
      Taking input and printing output is handled automatically.
   */
 
+      if(size==0){
+          return false;
+      }
+      else{
+          if(arr[size]==x)
+          return true;
 
-
+          else{
+           return checkNumber(arr, size-1,x);
+      }    
+      }
 }
-
 int main(){
     int n;
     cin >> n;
