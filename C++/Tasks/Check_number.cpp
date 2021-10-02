d42a5d73246c59afd89074e2ab33e3afb88d5d4c
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-02 19:22:23 +0530

    Update Check_number.cpp

--- C++/Tasks/Check_number.cpp
+++ C++/Tasks/Check_number.cpp
@@ -1,35 +1,29 @@
 #include<iostream>
 using namespace std;
 
-//Given an array of length N and an integer x, you need to find if x is present in the array or not. 
+//Given an array of length N and an integer x, you need to find if x is present in the array or not.
 //Return true or false.  Do this recursively.
 
 bool checkNumber(int input[], int size, int x) {
-  /* Don't write main().
-     Don't read input, it is passed as function argument.
-     Return output and don't print it.
-     Taking input and printing output is handled automatically.
-  */
-
-
-
+	if (size < 0) return false;
+	if (input[size] == x) return true;
+	return checkNumber(input, size - 1, x);
 }
 
-int main(){
-    int n;
-    cin >> n;
-    int *input = new int[n];
-    for(int i = 0; i < n; i++) {
-        cin >> input[i];
-    }
-    int x;
-    cin >> x;
-    if(checkNumber(input, n, x)) {
-        cout << "true" << endl;
-    }
-    else {
-        cout << "false" << endl;
-    }
+int main() {
+	int n;
+	cin >> n;
+	int *input = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> input[i];
+	}
+	int x;
+	cin >> x;
+	if (checkNumber(input, n, x)) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}
 }
-
 
