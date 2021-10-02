7dc24d7ea4a7248bd12d425c567a4f9839b179b7
chiragshivam <>
2021-10-02 13:10:58 +0000

    update maximum length bitonic subarray

--- C++/Tasks/maximum_length_bitonic_subarray.cpp
+++ C++/Tasks/maximum_length_bitonic_subarray.cpp
@@ -44,7 +44,33 @@ using namespace std;
 
 int bitonic(int arr[], int n)
 {
-   
+    if (n == 0)
+        return 0;
+         
+    int maxLen=1;
+         
+    int start=0;
+    int nextStart=0;
+         
+    int j =0;
+    while (j < n-1)
+    {
+        while (j<n-1 && arr[j]<=arr[j+1])
+            j++;
+             
+        while (j<n-1 && arr[j]>=arr[j+1]){
+            if (j<n-1 && arr[j]>arr[j+1])
+                nextStart=j+1;
+                 
+            j++;
+        }
+             
+        maxLen = max(maxLen,j-(start-1));
+             
+        start=nextStart;
+    }
+         
+    return maxLen;
 
 }
 
