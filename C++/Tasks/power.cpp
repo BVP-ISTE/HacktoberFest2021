c99b6b193ac6544e7d03edfd3cdfb928726cc264
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2021-10-03 14:29:50 +0530

    Update power.cpp
    I have completed the function and successfully executed it

--- C++/Tasks/power.cpp
+++ C++/Tasks/power.cpp
@@ -11,9 +11,10 @@ int power(int x, int n) {
      Return output and don't print it.
      Taking input and printing output is handled automatically.
   */
-
-
-
+    if (n == 0)
+   return 1;
+   else
+   return (x * power(x, n-1));
 }
 
 int main(){
@@ -22,5 +23,4 @@ int main(){
   
     cout << power(x, n) << endl;
 }
-
 
