85176728a6ba80b39657aa5f9d2f4dd667dc80da
unknown <arpitbirsingh65@gmail.com>
2021-10-03 20:21:27 +0530

    implement power of number

--- C++/Tasks/power.cpp
+++ C++/Tasks/power.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-//Write a program to find x to the power n (i.e. x^n). 
+//Write a program to find x to the power n (i.e. x^n).
 //Take x and n from the user. You need to return the answer.
 //Do this recursively.
 
@@ -11,15 +11,20 @@ int power(int x, int n) {
      Return output and don't print it.
      Taking input and printing output is handled automatically.
   */
-
-
+    if(x==0) return 0;
+    int power=1;
+    for(int i=0;i<n;i++)
+    {
+        power=power*x;
+    }
+    return power;
 
 }
 
 int main(){
     int x, n;
     cin >> x >> n;
-  
+
     cout << power(x, n) << endl;
 }
 
