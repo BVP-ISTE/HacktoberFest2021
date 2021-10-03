cc28f5805424474b777f24c7de441a254b71d62b
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-03 13:38:40 +0530

    Update Check if frequency of each digit is less than the digit.cpp
    @SohilK03

--- C++/Errors/Check if frequency of each digit is less than the digit.cpp
+++ C++/Errors/Check if frequency of each digit is less than the digit.cpp
@@ -1,42 +1,42 @@
-// A C++ program to validate a number 
-#include<bits/stdc++.h> 
-using namespace std; 
-  
-// Function to validate number (Check if 
-// frequency of a digit is less than the 
-// digit itself or not) 
-bool validate(long long int n) 
+// A C++ program to validate a number
+#include<bits/stdc++.h>
+using namespace std;
+
+// Function to validate number (Check if
+// frequency of a digit is less than the
+// digit itself or not)
+bool validate(long long int n)
 //find the error in bool function
-{ 
-    for (int i=0; i<10; i++) 
-    { 
-        long long int temp = n;  
-        int count = 10; 
-        while (temp) 
-        { 
-            // If current digit of temp is 
-            // same as i 
-            if (temp % 10 == i) 
-                count++; 
-  
-            // if frequency is greater than  
-            // digit value, return false 
-            if (count > i) 
-                return false; 
-  
-            temp /= 10; 
-        } 
-    } 
-    return true; 
-} 
-  
-// driver program 
-int main() 
-{ 
-    long long int n = 1552793; 
-    if (validate(n)) 
-        cout << "True"; 
-    else
-        cout << "False"; 
-    return 0; 
-}  
+{
+	for (int i = 0; i < 10; i++)
+	{
+		long long int temp = n;
+		int count = 0;
+		while (temp)
+		{
+			// If current digit of temp is
+			// same as i
+			if (temp % 10 == i)
+				count++;
+
+			// if frequency is greater than
+			// digit value, return false
+			if (count > i)
+				return false;
+
+			temp /= 10;
+		}
+	}
+	return true;
+}
+
+// driver program
+int main()
+{
+	long long int n = 1552799;
+	if (validate(n))
+		cout << "True";
+	else
+		cout << "False";
+	return 0;
+}
