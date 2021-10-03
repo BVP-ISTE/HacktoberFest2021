4e4a10a73370559cbf37dfe53e8338e63a2dadf2
Prajwal Horti <58554461+hortiprajwal@users.noreply.github.com>
2021-10-03 12:50:24 +0530

    Update and rename Check_number.cpp to updated Check_number.cpp

--- C++/Tasks/updated Check_number.cpp
+++ C++/Tasks/updated Check_number.cpp
@@ -10,7 +10,17 @@ bool checkNumber(int input[], int size, int x) {
      Return output and don't print it.
      Taking input and printing output is handled automatically.
   */
-
+  int l=0,r=size-1;
+  while(l<=r){
+      int mid=l+(r-l)/2;
+      if(input[mid]==x)
+        return true;
+      else if(input[mid]<x)
+        l=mid+1;
+      else
+        r=mid-1;
+  }
+  return false;
 
 
 }
