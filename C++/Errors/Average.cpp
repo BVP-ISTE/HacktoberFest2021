d17680867ae934801e6d7d3ec281daa10714a443
Sonu Kumar Keshri <75078531+Sonukumarkeshri4@users.noreply.github.com>
2021-10-03 21:21:22 +0530

    Update Average.cpp (#219)

--- C++/Errors/Average.cpp
+++ C++/Errors/Average.cpp
@@ -10,7 +10,7 @@ int main()
  cin >> n;
 //error in the below segment
 //-------------
- for(i = 0:, i <= n:, ++i)
+ for(i = 0; i < n; ++i)
  {
  cout << i + 1 << ". Enter number: ";
  cin >> num[i];
