e52f8c5f0dd893ad4336e025931ee28415a62522
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-04 00:57:41 +0530

    Update GCD.cpp
    #26 
    @divya520357

--- C++/Errors/GCD.cpp
+++ C++/Errors/GCD.cpp
@@ -1,23 +1,23 @@
 //Program to find GCD
 
- 
+
 #include <iostream>
 using namespace std;
 int main()
 {
-int m,n;
- cout<<"Enter two no.s "<<endl;
- cin>>m>>n;
-  //error in the below segment
+	int m, n;
+	cout << "Enter two no.s " << endl;
+	cin >> m >> n;
+	//error in the below segment
 //-------------
-while(m!=n)
- {
- if(m>=n)
- m=m-n;
- else
- n=n-m;
- }
- cout<<"GCD is "<<m;
- //-------------
-return 0;
+	while (m != n and m != 0 and n != 0)
+	{
+		if (m >= n)
+			m = m - n;
+		else
+			n = n - m;
+	}
+	cout << "GCD is " << m;
+//-------------
+	return 0;
 }
