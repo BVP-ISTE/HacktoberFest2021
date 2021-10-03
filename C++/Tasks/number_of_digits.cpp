e22906080cda0ee81b011708ea8727cc09aa1612
bhardwajnishant1 <91819633+bhardwajnishant1@users.noreply.github.com>
2021-10-03 17:36:08 +0530

    Update number_of_digits.cpp
    #209 
    @SohilK03

--- C++/Tasks/number_of_digits.cpp
+++ C++/Tasks/number_of_digits.cpp
@@ -1,24 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Given the code to find out and return the number of digits present in a number recursively. 
+//Given the code to find out and return the number of digits present in a number recursively.
 //But it contains few bugs, that you need to rectify such that all the test cases should pass.
 
-int count(int n){
-    if(n == 0){
-        return 1;
-    }
-    int smallAns = count(n / 10);
-    return smallAns + 1;
+int count(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	int smallAns = count(n / 10);
+	return smallAns + 1;
 }
 
 //no changes to be made below this line.
 
-int main(){
-    int n;
-    cin >> n;
-  
-    cout << count(n) << endl;
+int main() {
+	int n;
+	cin >> n;
+
+	cout << count(n) << endl;
 }
-
-
