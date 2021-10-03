934192e93c5d3d6bb31de53b78009677e50c2616
Prajwal Horti <58554461+hortiprajwal@users.noreply.github.com>
2021-10-03 11:38:03 +0530

    Update and rename sum of digits.cpp to resolved errors in sum of digits.cpp

--- C++/Errors/resolved errors in sum of digits.cpp
+++ C++/Errors/resolved errors in sum of digits.cpp
@@ -1,18 +1,19 @@
 //Program for Sum of Digits of a Number
 #include <iostream>
 using namespace std;
+
 int main()
 {
 int n,r,sum=0;
- cout<<"Enter a no. ";
+ cout<<"Enter a no. :";
  cin>>n;
- //error in the below segment
+ //error resolved in the below segment
 //-------------
-while(n!==0)
+while(n!=0)
  {
- r=n/10;
- n=n/10;
- sum=sum+r;
+    r=n%10;
+    sum=sum+r;
+    n=n/10;
  }
  //-------------
  cout<<sum;
