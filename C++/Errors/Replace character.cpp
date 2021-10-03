6a0a41354770ba2feb8aaa7eabe7f13ad58bb27a
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-03 14:43:10 +0530

    Update Replace character.cpp (#213)
    #64 
    @SohilK03

--- C++/Errors/Replace character.cpp
+++ C++/Errors/Replace character.cpp
@@ -5,23 +5,22 @@ using namespace std;
 
 void replaceCharacter(char input[], char c1, char c2) {
 
-    int i,count=0;
-    for(i=0;input[i]!='\0';i++){
-        count++;
-    }
-    int size=count;
-    for(int j=0;j<size;j++){
-        if(input[j]==c1){
-            
-        }
-    }
+	int i, count = 0;
+	for (i = 0; input[i] != '\0'; i++) {
+		count++;
+	}
+	int size = count;
+	for (int j = 0; j < size; j++) {
+		if (input[j] == c1) {
+			input[j] = c2;
+		}
+	}
 }
 int main() {
-    char input[1000000];
-    char c1, c2;
-    cin >> input;
-    cin >> c1 >> c2;
-    replaceCharacter(input, c1, c2);
-    cout << input << endl;
+	char input[1000000];
+	char c1, c2;
+	cin >> input;
+	cin >> c1 >> c2;
+	replaceCharacter(input, c1, c2);
+	cout << input << endl;
 }
-
