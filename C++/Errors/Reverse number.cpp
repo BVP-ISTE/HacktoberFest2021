a3b42c037c1bea28b84ede1c116b9adaad005d1d
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2021-10-04 20:38:21 +0530

    Update Reverse number.cpp (#256)

--- C++/Errors/Reverse number.cpp
+++ C++/Errors/Reverse number.cpp
@@ -13,14 +13,14 @@ int main()
 //-------------
  do
  {
- digit = num / 10;
+ digit = num % 10;
  rev = (rev * 10) + digit;
  num = num / 10;
  } while (num != 0);
 
  cout << " The reverse of the number is: " << rev << endl;
 
- if (n = rev)
+ if (n == rev)
  cout << " The number is a palindrome";
  else
  cout << " The number is not a palindrome";
