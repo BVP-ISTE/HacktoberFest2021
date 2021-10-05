4a2863197fece499366741b1613571887ea80048
abhishekr0 <77566530+abhishekr0@users.noreply.github.com>
2021-10-05 20:26:19 +0530

    Update check permutation.cpp (#283)
    #62 
    @divya520357

--- C++/Errors/check permutation.cpp
+++ C++/Errors/check permutation.cpp
@@ -4,43 +4,29 @@
 using namespace std;
 
 bool isPermutation(char input1[], char input2[]) {
-    int len1=strlen(input1);
-    int len2=strlen(input2);
-
-    if(len1!=len2){
-        return false;
-    }
-
-    int i,j,count=0;
-    for(i=0;i<len1;i++){
-        for(j=0;j<len2;j++){
-            if(input1[i]==input2[j]){
-                count++;
-               
-
-            }
-        }
-
-    }
-
-    if(count==len1){
-        return true;
-    }
-
-    else{
-        return false;
-    }
+	int len1 = strlen(input2);
+	int len2 = strlen(input1);
+	if (len1 != len2) {
+		return false;
+	}
+	//sorted both string alphabetical order.
+	sort(input1, input1 + len1);
+	sort(input2, input2 + len2);
+	//if two sorted string have same character then it's permutaion of each other.
+	if (strcmp(input1, input2) == 0) return true;
+	//else their are not permutaion.
+	return false;
 }
+
 
 int main() {
-    char input1[1000], input2[1000];
-    cin.getline(input1, 1000);
-    cin.getline(input2, 1000);
-    if(isPermutation(input1, input2) == 1) {
-        cout << "true";
-    }
-    else {
-        cout << "false";
-    }
+	char input1[1000], input2[1000];
+	cin.getline(input1, 1000);
+	cin.getline(input2, 1000);
+	if (isPermutation(input1, input2) == 1) {
+		cout << "true";
+	}
+	else {
+		cout << "false";
+	}
 }
-
