5ce9923385cbb706eb3a97447ec806af768f37ff
unknown <arpitbirsingh65@gmail.com>
2021-10-05 21:14:40 +0530

    Remove errors from calculate_the_power_of_number.py

--- Python/Tasks/calculate_the_power_of_number.py
+++ Python/Tasks/calculate_the_power_of_number.py
@@ -2,15 +2,15 @@
 
 # Write a Python program to calculate the value of 'a' to the power 'b'.
 
-def power(a,b)
-	if b==0
+def power(a,b):
+	if b==0:
 		return 1
-	elif a==0
+	elif a==0:
 		return 0
-	elif b==1
+	elif b==1:
 		return a
-	else
-		return a*power(a,b-1);
+	else:
+		return a*power(a,b-1)
 
 print(power(2,5))
 
@@ -18,10 +18,10 @@ print(power(2,5))
 # Write a Python program to calculate the sum of a list of numbers
 
 def list_sum(num_List):
-    if len((num_List) == 1)
+    if (len(num_List) == 1):
         return num_List[0]
-    else
-        return num_List[0] + list_sum(num_List[1:]
+    else:
+        return num_List[0] + list_sum(num_List[1:])
         
 print(list_sum([2, 4, 5, 6, 7]))
 
