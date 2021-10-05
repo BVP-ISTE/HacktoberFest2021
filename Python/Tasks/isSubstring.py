7c6f1a07c9ebfeaa2e7ff09661f347536e50c472
Gulshan kumar Jakhon <72978519+gulshanjakhon@users.noreply.github.com>
2021-10-06 00:47:40 +0530

    problem_solved

--- Python/Tasks/isSubstring.py
+++ Python/Tasks/isSubstring.py
@@ -1,6 +1,9 @@
 # program to find substring in string
 
 def isSubstring(s,s1):
+  if s1 in s:
+      return True
+  else:
+    return False
 
-
-print isSubstring("anumsharma","ar")
+print(isSubstring("anumsharma","ar"))
