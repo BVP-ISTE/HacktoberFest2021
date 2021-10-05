9e2c37418bd3e5a96b940df11c682ccd73019b76
bhardwajnishant1 <91819633+bhardwajnishant1@users.noreply.github.com>
2021-10-05 21:55:42 +0530

    Update unique element.cpp (#263)
    #54 
    @divya520357

--- C++/Errors/unique element.cpp
+++ C++/Errors/unique element.cpp
@@ -5,48 +5,48 @@ using namespace std;
 
 int findUnique(int *arr, int size)
 {
-    int i,j;
-    bool num=true;
-    for(i=0;i<size;i++){
-        bool f=false;
-        for(j=0;j<size;j++){
-            if(j==i){
-                continue;
-            }
-            if(arr[i]==arr[j]){
-                
-                break;
-            }
+  int i, j;
+  bool num = true;
+  for (i = 0; i < size; i++) {
+    bool f = false;
+    for (j = i + 1; j < size; j++) {
+      if (j == i) {
+        continue;
+      }
+      if (arr[i] == arr[j]) {
+        f = true;
+        break;
+      }
 
-        }
-
-        if(f==false){
-            return arr[i];
-        }
-
-        }
     }
+
+    if (f == false) {
+      return arr[i];
+    }
+
+  }
+}
 
 
 
 int main()
 {
 
-	int t;
-	cin >> t;
+  int t;
+  cin >> t;
 
-	while (t--)
-	{
-		int size;
-		cin >> size;
-		int *input = new int[size];
+  while (t--)
+  {
+    int size;
+    cin >> size;
+    int *input = new int[size];
 
-		for (int i = 0; i < size; ++i)
-		{
-			cin >> input[i];
-		}
-		cout << findUnique(input, size) << endl;
-	}
+    for (int i = 0; i < size; ++i)
+    {
+      cin >> input[i];
+    }
+    cout << findUnique(input, size) << endl;
+  }
 
-	return 0;
+  return 0;
 }
