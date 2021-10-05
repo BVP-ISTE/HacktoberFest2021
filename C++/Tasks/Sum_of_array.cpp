d5b65750ad8770c9c3cd2604db4e924bd445479d
simar <simar@Simars-MacBook-Air.local>
2021-10-05 19:12:41 +0530

    Added task for sum of array

--- C++/Tasks/Sum_of_array.cpp
+++ C++/Tasks/Sum_of_array.cpp
@@ -1,17 +1,12 @@
 #include<iostream>
 using namespace std;
 
-//Given an array of length N, you need to find and return the sum of all elements of the array.
-//Do this recursively.
-
 int add;
 
 int sum(int input[], int n) {
-  /* Don't write main().
-     Don't read input, it is passed as function argument.
-     Return output and don't print it.
-     Taking input and printing output is handled automatically.
-  */
+  if (n <= 0)
+        return 0;
+    return (sum(input, n - 1) + input[n - 1]);
 
 
 }
@@ -28,6 +23,7 @@ int main(){
     }
     
     cout << sum(input, n) << endl;
+    return 0;
 }
 
 
