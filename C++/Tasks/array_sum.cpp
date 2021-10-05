a8683edd315f31035e47df5032a91b4ccb1b1b57
Gulshan kumar Jakhon <72978519+gulshanjakhon@users.noreply.github.com>
2021-10-06 00:05:30 +0530

    update problem solved

--- C++/Tasks/array_sum.cpp
+++ C++/Tasks/array_sum.cpp
@@ -3,13 +3,15 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter length of Array : ";
     cin>>n;
 
     int input[n];
+    cout<<"Enter elements one by one :\n"<<endl;
     for(int i=0; i<n; i++){
         cin>>input[i];
     }
-    
+
     /*
     You have to write the code for sum of array
     */
@@ -17,11 +19,9 @@ int main(){
     for(int i = 0; i<n ; i++){
 
         sum+=input[i];
-        
+
     }
 
-    cout<<sum<<endl;
+    cout<<"Sum of Elements of array is : "<<sum<<endl;
 
 }
-
-
