3815677f47d5ac41190d815b002c11ca22a75843
Manomay Kagalkar <manomay111@gmail.com>
2021-10-06 22:53:20 +0530

    Wrote binary_numbers.py from scratch

--- Python/Tasks/binary_numbers.py
+++ Python/Tasks/binary_numbers.py
@@ -7,18 +7,33 @@ Example:
 Then the output should be:
 1010
 """
-value = []
-items={x for x in raw_input().split(',')}
-for p in items:
-    intp = int(p, 2)
-    if not intp%5
-        value.append(p)
-
-print ','.joinvalue
 
 
-"""
-output:
-1011,1101,1010,1001
-1010
-"""
+def binary_to_decimal(bi):
+    n1 = int(bi[0])
+    n2 = int(bi[1])
+    n3 = int(bi[2])
+    n4 = int(bi[3])
+    num = (n1 * 8) + (n2 * 4) + (n3 * 2) + (n4 * 1)
+    return num
+
+
+def binary_numbers():
+    print("Please enter comma seperated binary numbers")
+    s = input()
+    i = 0  # loop variable
+    noc = s.count(",")  # number of commas in original input
+    noc += 1
+    while i <= noc:
+        comma = s.find(",")  # position of next comma
+        if comma == -1:
+            break
+        b = s[0: (comma+1)]  # binary number extracted
+        d = binary_to_decimal(b)
+        if d % 5 == 0:
+            print(b.replace(",", ""))
+        s = s.replace(b, "")
+        i += 1
+
+
+binary_numbers()
