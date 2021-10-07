948e5359e8fa0ad0d947a2749c6913990ab4709d
Xavier Jouvenot <x.jouvenot@expressivee.com>
2021-10-07 18:05:49 +0200

    Corrects the errors in C++/Errors/UpperLowerDirty.cpp (#300)
    * Corrects the source code of "C++/Errors/UpperLowerDirty.cpp" in order to make it compile and actually do what is intended:
    Displays the specified string in upper case and lower case using the strupr and strlwr functions
    
    * Removes unecessary temp variable.

--- C++/Errors/UpperLowerDirty.cpp
+++ C++/Errors/UpperLowerDirty.cpp
@@ -1,16 +1,20 @@
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <cstring>
+
 using namespace std;
- 
+
 int main()
 {
-char str1 [30], temp[30];
-cout < "Enter the string:";
-cin > str1;
-strcpy ( temp, str1);
-cout < < "strupr ( tmp ) : " < < strupr (tmp) < < end1
-cout < < "strlwr (tmp) : " < < strlwr ( tmp) < < end1
+char str1 [30];
+cout << "Enter the string:";
+cin >> str1;
+
+strupr(str1);
+cout << "strupr (tmp) : " << str1 << endl;
+
+strlwr(str1);
+cout << "strlwr (tmp) : " << str1 << endl;
 system("pause");
  
 return 0;
