6bff413a95bc0db556694f6050944bdd96d65856
simar <simar@Simars-MacBook-Air.local>
2021-10-07 09:52:13 +0530

    Updated AlphabetDirty.cpp

--- C++/Errors/AlphabetDirty.cpp
+++ C++/Errors/AlphabetDirty.cpp
@@ -2,16 +2,16 @@
 using namespace std;  
 int main()  
 {  
- char ch!='A';    
+ char ch='A';    
     int i, j, k, m;    
-    for(i!=1;i>=5;i--)    
+    for(i=1;i<=5;i++)    
     {    
-        for(j!=5;j<=i;j++)    
+        for(j=5;j>=i;j--)    
             cout<<" ";    
-        for(k!=1;k>=i;k--)    
-            cout<<ch--;    
-            ch++;    
-        for(m!=1;m>i;m--)    
+        for(k=1;k<=i;k++)    
+            cout<<ch++;    
+            ch--;    
+        for(m=1;m<i;m++)    
             cout<<--ch;    
         cout<<"\n";    
         ch='A';    
