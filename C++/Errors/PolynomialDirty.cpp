9f8607abf563bf10219ab2dd5f74588796fb3577
Xavier Jouvenot <x.jouvenot@expressivee.com>
2021-10-07 17:30:52 +0200

    Corrects the bugs in C++/Errors/PolynomialDirty.cpp (#299)
    * Adds a sanity check on the degree specified by the user, in order to avoid overflows in the array used.
    
    * Defines the variable "i" only when needed (in the for loop)
    
    * Corrects the implementation of the poly function.

--- C++/Errors/PolynomialDirty.cpp
+++ C++/Errors/PolynomialDirty.cpp
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <conio.h>
+#include <cmath>
 
 float poly(float a[], int, float);
 
 int main()
 {
 	float x, a[10], y1;
-	int deg, i;
+	int deg;
 
-	printf("Enter the degree of polynomial equation: ");
+	printf("Enter the degree of polynomial equation (less than 10): ");
 	scanf("%d", &deg);
+
+	if(deg >= 10)
+	{
+		printf("Error ! You must specify a degree under 10.");
+		return 1;
+	}
 
 	printf("Enter the value of x for which the equation is to be evaluated: ");
 	scanf("%f", &x);
 
-	for(i=0; i<=deg; i++)
+	for(int i=0; i<=deg; i++)
 	{
 		printf("Enter the coefficient of x to the power %d: ",i);
 		scanf("%f",&a[i]);
@@ -30,14 +37,12 @@ int main()
 /* function for finding the value of polynomial at some value of x */
 float poly(float a[], int deg, float x)
 {
-	float p;
-	int i;
+	float p = 0.f;
 	
-	p = a[deg];
-	
-	for(i=deg;i>=1;i--)
+	// Polynomial function: a_{n}x^{n}+a_{n-1}x^{n-1}+ ... +a_{2}x^{2}+a_{1}x+a_{0}
+	for(int i=deg; i>=0; i--)
 	{
-		p = (a[i-1] + x*p);
+		p += a[i] * std::pow(x, i);
 	}
 	
 	return p;
