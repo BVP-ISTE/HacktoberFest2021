5e07d90de0f7f1e8626a62ca93a980611f72cffc
mdsarfarazalam840 <41156047+mdsarfarazalam840@users.noreply.github.com>
2021-10-08 10:48:08 +0530

    Substring task fixed

--- Java/Tasks/substring task.java
+++ Java/Tasks/substring task.java
@@ -7,10 +7,26 @@ class Main {
 
     // check if substring is present or not(i.e. str1 and str2)
     
-    //check for both the strings and take the help of conditional statements..
+    
     
     //write your code here..
+    boolean result = txt.contains(str1);
     
+    //check for both the strings and take the help of conditional statements..
+    if(result) {
+      System.out.println(str1 + " is present in the string.");
+    }
+    else {
+      System.out.println(str1 + " is not present in the string.");
+    }
+
+    result = txt.contains(str2);
+    if(result) {
+      System.out.println(str2 + " is present in the string.");
+    }
+    else {
+      System.out.println(str2 + " is not present in the string.");
+    }
     
     
   }
