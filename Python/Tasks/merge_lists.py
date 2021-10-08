3b94fd18d917be7b547ba7e6fbc7f6dfd7b50c51
william chandra <williamchans57@gmail.com>
2021-10-08 10:28:32 +0000

    Update Python/Tasks/merge_lists.py

--- Python/Tasks/merge_lists.py
+++ Python/Tasks/merge_lists.py
@@ -10,18 +10,17 @@ def merge_lists(my_list, alices_list):
            while current_index_merged < merged_list_size:
                     is_my_list_exhausted = current_index_mine >= len(my_list)
                     is_alices_list_exhausted = current_index_alices >= len(alices_list)
-
-                    if not is_my_list_exhausted and (is_alices_list_exhausted or (my_list[current_index_mine] < ):
-                           
-
+                    if not is_my_list_exhausted and (is_alices_list_exhausted or (my_list[current_index_mine] < alices_list[current_index_alices])):
+                           merged_list[current_index_merged] = my_list[current_index_mine]
+                           current_index_mine += 1
                     # case: next comes from Alice's list
                     else:
-                            
-
-           current_index_merged += 1
+                           merged_list[current_index_merged] = alices_list[current_index_alices]
+                           current_index_alices += 1
+                    current_index_merged += 1
 
            return merged_list
 
 list1=[2,4,6,8]
 list2=[3,5,7,9]
-print merge_lists(list1,list2)
+print(merge_lists(list1,list2))
