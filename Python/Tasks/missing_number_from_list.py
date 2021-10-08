8991c11aaddd57e7feeb8e8989ded267b4ce8579
CinematicGenius007 <75163638+CinematicGenius007@users.noreply.github.com>
2021-10-08 17:52:19 +0530

    Completed the task
    For hacktoberfest2021.

--- Python/Tasks/missing_number_from_list.py
+++ Python/Tasks/missing_number_from_list.py
@@ -1,7 +1,7 @@
 #program to find a missing number from a list.
 
 def missing_number(num_list):
-    return sum(range()
+    return sum(range(min(num_list),max(num_list)+1)) - sum(num_list) # finding that missing number in terms of difference
 
 print(missing_number([1,2,3,4,6,7,8]))  #5
 
