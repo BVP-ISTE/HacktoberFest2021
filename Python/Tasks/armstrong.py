23b25919ccdb67628b6b08f59b177e9685297b49
mdsarfarazalam840 <41156047+mdsarfarazalam840@users.noreply.github.com>
2021-10-09 11:10:42 +0530

    Code Fixed armstrong.py

--- Python/Tasks/armstrong.py
+++ Python/Tasks/armstrong.py
@@ -13,10 +13,10 @@ for num in range(lower, upper + 1)
 
    # find the sum of the cube of each digit
    temp = num
-   while (temp > 0)
+   while temp>0:
        digit = temp % 10
        sum += digit ** order
-       temp /= 10
+       temp //= 10
 
-   if num == sum
+   if num == sum:
        print(num)
