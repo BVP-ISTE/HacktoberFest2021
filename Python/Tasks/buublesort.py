6806b98177dadd92e4d18296ea03865379c8baf1
CinematicGenius007 <75163638+CinematicGenius007@users.noreply.github.com>
2021-10-09 16:43:42 +0530

    corrected the bubbleSort.py
    For hacktoberfest2021, thanks for the opportunity.

--- Python/Tasks/buublesort.py
+++ Python/Tasks/buublesort.py
@@ -3,27 +3,27 @@
 
 def bubbleSort(list):
     for passnum in range(len(list)-1,0,-1):
-        for i in range():
+        for i in range(0,passnum):
             if list[i]>list[i+1]:
                 temp=list[i]
-            
+                list[i]=list[i+1] # added this line
                 list[i+1]=temp
 
 
 alist= list()
-number=raw_input("enter the elements")
+number=raw_input("enter the elements ") # also raw_input
 
-for i in range(int(number):
-    n=raw_input("number: ");
-    alist.appendint(n)
+for i in range(int(number)):
+    n=raw_input("number: ")
+    alist.append(int(n))
 
-print "the unsorted array"
+print("the unsorted array") # I used print "the... " but that didn't work. So, I used parenthesis instead
 
-print alist
+print(alist)
 
-bubbleSort(alist);
+bubbleSort(alist)
 
-print "sorted array"
+print("sorted array")
 
-print alist
+print(alist)
     
