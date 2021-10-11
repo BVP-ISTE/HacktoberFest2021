8d5a8051384936f464f1d23e3b8be7097bbaf835
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2021-10-11 18:22:02 +0530

    Array Sorting

---
+++ C++/Tasks/Selection_Sort.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin>>n;
+
+    int arr[100]; // change this to n
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            if (arr[j]<arr[i])
+            {
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            }
+            
+        }
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+}
