f91f5c7631728c78ff4c635d1b1bfdbcb14e10e7
Priyanshi Srivastava <65450458+Priyanshi-S@users.noreply.github.com>
2021-10-12 22:59:35 +0530

    Update stringispalindrome.py

--- Python/Tasks/stringispalindrome.py
+++ Python/Tasks/stringispalindrome.py
@@ -3,11 +3,13 @@
 # 1st method - making the string length half
 
 def is_palindrome(string):
-    
+    str_len= len(string);
+    half_len= int(str_len/2);
+    result=True
     for i in range(0, half_len):
       # you need to check only half of the string
-        print string[str_len-i-1]
-        if :
+        #print(string[str_len-i-1])
+        if string[i]!= string[str_len-i-1]:
             result = False
             break
     return result 
@@ -17,9 +19,8 @@ print is_palindrome("abcrtba")
 '''
 
 # 2nd method
-
 def palindrome(n):
     return n == n[:: -1]
 
-print palindrome("kanak")
+print(palindrome("kanak"))
 '''
