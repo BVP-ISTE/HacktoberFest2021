78d919a4daa9b65727720e7ddf630f2ff2f9ac49
Vividh Pandey <91251535+VividhPandey003@users.noreply.github.com>
2021-10-13 22:53:23 +0530

    Update palindrome.py

--- Python/Tasks/palindrome.py
+++ Python/Tasks/palindrome.py
@@ -2,11 +2,13 @@ def palindrome(n):
   m=n 
   a=0
   while(m!=0):
-    
+      d=m%10
+      a=a*10+d
+      m=m//10    
     
   if (n == a):
     return "it is a palindrome number"
   else:
     return "it is not palindrome number"
     
-print palindrome(156)
+print (palindrome(156))
