203f36934279c1d617346d75ea56383622ce6471
prajna-spec <64818027+prajna-spec@users.noreply.github.com>
2021-10-14 16:18:08 +0530

    Updated_pair_sum
    The time complexity is much lesser now. It is O(n).
    This is a very optimised approach
    
    The function returns the count of the number of pairs where their sum is equal to the given target

--- C++/Tasks/pair_sum_in_array.cpp
+++ C++/Tasks/pair_sum_in_array.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 //You have been given an integer array/list(ARR) and a number X. 
 //Find and return the total number of pairs in the array/list which sum to X.
 
-int pairSum(int *input, int size, int x){
-	//Write your code here
-  	int count = 0; 
+//updated code with a much lesser time complexity
 
-	    for (int i = 0; i < size; i++)
-		for (int j = i + 1; j < size; j++)
-		    if (input[i] + input[j] == x)
-			count++;
-
-	    return count;
+int pairSum(int arr[], int n, int k)
+{
+    unordered_map<int, int> m;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (m.find(k - arr[i]) != m.end()) {
+            count += 1;
+        }
+        m[arr[i]]++;
+    }
+    return count;
 }
+
 
 //all changes to be made in the above function only
 //no changes to be made below this line
@@ -39,9 +44,12 @@ int main()
 
 		cin >> x;
 
-		cout << pairSum(input, size, x) << endl;
+        int result;
 
-		delete[] input;
+		result= pairSum(input, size, x) ;
+        cout<<result<<endl;
+
+		
 	}
 	
 	return 0;
