4b48a639cc311d1e4dc46b4ab14755ca3040419b
Aditya Ojha <adityaojha.wr10@gmail.com>
2021-10-17 00:40:34 +0530

    issue91 solved

--- Python/Tasks/reverseNumber.py
+++ Python/Tasks/reverseNumber.py
@@ -1,9 +1,10 @@
 # reverse the number
 
 def reverseNumber(n):
-  Reverse = 0
-  while(n > 0):
-    
-  return Reverse
- 
-print reverseNumber(1565)
+	Reverse = 0
+	while(n > 0):
+		Remainder = n % 10
+		Reverse = Reverse * 10 + Remainder
+		n //= 10
+	return Reverse
+print(reverseNumber(1565))
