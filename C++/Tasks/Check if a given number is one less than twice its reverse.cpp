b42d0b6e28f79355465ac43b433e90f7fdf73cf5
priyanshu kumar <58341603+priyanshu294@users.noreply.github.com>
2021-10-23 12:01:55 +0530

    Update Check if a given number is one less than twice its reverse.cpp

--- C++/Tasks/Check if a given number is one less than twice its reverse.cpp
+++ C++/Tasks/Check if a given number is one less than twice its reverse.cpp
@@ -1,40 +1,39 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-// Iterative function to 
-// reverse digits of num 
-int rev(int num) 
-{ 
-    int rev_num = 0; 
-  
-    // Loop to extract all 
-    // digits of the number 
-    while (num > 0) { 
-        rev_num 
-            = rev_num * 10 + num % 10; 
-        num = num / 10; 
-    } 
-    return rev_num; 
-} 
-  
-// Function to check if N 
-// satisfies given equation 
-bool check(int n) 
-{ 
-   //complete bool function
-   
-    int r_n = rev(n);
-    return n==2*r_n-1;
-    
-} 
-  
-// Driver Code 
-int main() 
-{ 
-    int n = 73; 
-    if (check(n)) 
-        cout << "Yes"; 
-    else
-        cout << "No"; 
-    return 0; 
+// C++ program of the
+// above approach
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Iterative function to
+// reverse digits of num
+int rev(int num)
+{
+	int rev_num = 0;
+
+	// Loop to extract all
+	// digits of the number
+	while (num > 0) {
+		rev_num
+			= rev_num * 10 + num % 10;
+		num = num / 10;
+	}
+	return rev_num;
+}
+
+// Function to check if N
+// satisfies given equation
+bool check(int n)
+{
+	return 2 * rev(n) == n + 1;
+}
+
+// Driver Code
+int main()
+{
+	int n = 73;
+	if (check(n))
+		cout << "Yes";
+	else
+		cout << "No";
+	return 0;
 }
