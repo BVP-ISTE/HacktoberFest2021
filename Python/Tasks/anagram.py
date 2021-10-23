f64d7f586e71e4dad5c1d64e727892d9e37d2dbb
priyanshu kumar <58341603+priyanshu294@users.noreply.github.com>
2021-10-23 11:37:59 +0530

    Update anagram.py

--- Python/Tasks/anagram.py
+++ Python/Tasks/anagram.py
@@ -1,32 +1,22 @@
-# ANAGRAM FINDER
+str1 = "Race"
+str2 = "Care"
 
-def anagram(arr):
-    ''' This function takes in the original list of strings, converts each of
-the strings to lists of characters and sorts them in alphabetic order. By
-sorting them its clear which of the words are anagrams'''
-    sorted_list = []
-    for i in arr:
-        #the sorted function returns a sorted list
-        i = sorted(i)
-        sorted_list.append(i)
-    return sorted_list
+# convert both the strings into lowercase
+str1 = str1.lower()
+str2 = str2.lower()
 
+# check if length is same
+if(len(str1) == len(str2)):
 
-def anagram_finder(arr):
-    super_list = []
-    sorted_list = anagram(arr)
-    for i in range(len(sorted_list)):
-        #words that have no anagrams are placed in their separate sublists
-        #that is words that appear once when sorted
-        if sorted_list.count(sorted_list[i]) == 1:
-            super_list.append([arr[i]])
-        for j in range(len(sorted_list)):
-            if sorted_list[i] == sorted_list[j] and i != j:
-                #words that appear twice are anagrams and are placed in their sublists
-                if [arr[i], arr[j]] not in super_list and [arr[j], arr[i]] not in super_list:
-                    super_list.append([arr[i], arr[j]])
+    # sort the strings
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
 
-    return super_list
+    # if sorted char arrays are same
+    if(sorted_str1 == sorted_str2):
+        print(str1 + " and " + str2 + " are anagram.")
+    else:
+        print(str1 + " and " + str2 + " are not anagram.")
 
-
-print(anagram_finder(['cat', 'race', 'tac', 'care', 'bus', 'dog', 'road', 'fried', 'fired']))
+else:
+    print(str1 + " and " + str2 + " are not anagram.")
