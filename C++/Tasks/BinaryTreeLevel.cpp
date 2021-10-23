8a0f325d332ed20365bd7ff6b83d6d6abf00b67e
priyanshu kumar <58341603+priyanshu294@users.noreply.github.com>
2021-10-23 12:02:56 +0530

    Update BinaryTreeLevel.cpp

--- C++/Tasks/BinaryTreeLevel.cpp
+++ C++/Tasks/BinaryTreeLevel.cpp
@@ -1,124 +1,46 @@
-//Find the level in a binary tree with given sum K
-
-// C++ program to print all
-// K-sum levels in a Binary Tree
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-
-// Vector to store the
-// elements of a level
-vector<int> level;
-
-// Binary Tree Node
-struct node {
-	struct node* left;
-	int data;
-	struct node* right;
+//structure of Node
+struct Node{
+  int data;
+  Node* left;
+  Node* right;
 };
-
-// Function to display elements
-void display(bool flag)
-{
-
-	// Check if boolean variable is true
-	// then print the level
-	if (flag) {
-
-		for (auto x : level)
-			cout << x << " ";
-	}
-
-	else
-
-		cout << "Not Possible\n";
+//function to create the node
+Node* create(int x){
+  Node* temp=new Node;
+  temp->data=x;
+  temp->left=NULL;
+  temp->right=NULL;
+  return temp;
 }
-
-// Function to find sum of
-// elements by level order traversal
-void SumlevelOrder(node* root, int k)
+//function to find the level of node
+int find(Node* root,int t,int l)
 {
-
-	if (root == NULL)
-		return;
-
-	// Queue data structure for
-	// level order traversal
-	queue<node*> q;
-
-	// Enqueue Root in Queue
-	q.push(root);
-
-	bool flag = false;
-
-	while (q.empty() == false) {
-
-		// number of nodes at current level
-		int nodeCount = q.size();
-
-		int Present_level_sum = 0;
-
-		// Dequeue all nodes of current level and
-		// Enqueue all nodes of next level
-		while (nodeCount > 0) {
-
-			node* node = q.front();
-
-			// To add node data
-			Present_level_sum += node->data;
-
-			level.push_back(node->data);
-
-			q.pop();
-
-			if (node->left != NULL)
-				q.push(node->left);
-
-			if (node->right != NULL)
-				q.push(node->right);
-
-			nodeCount--;
-		}
-
-		if (Present_level_sum == k) {
-
-			flag = true;
-			break;
-		}
-
-		level.clear();
-	}
-
-	display(flag);
+    if(!root)return 0;
+    if(root->data==t)return l;
+    int x=find(root->left,t,l+1);
+    if(x!=0)return x;
+    x=find(root->right,t,l+1);
+    return x;
 }
-
-// Function to create a new tree node
-node* newNode(int data)
+int getLevel(struct Node *node, int target)
 {
-	node* temp = new node;
-	temp->data = data;
-	temp->left = NULL;
-	temp->right = NULL;
-	return temp;
+    if(!node)return 0;
+  int l=1;
+    return find(node,target,l);
 }
-
-// Driver code
-int main()
-{
-	// Create binary tree
-	node* root = newNode(1);
-
-	root->left = newNode(2);
-	root->right = newNode(3);
-
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
-	root->right->right = newNode(6);
-
-	int K = 15;
-
-	SumlevelOrder(root, K);
-
-	return 0;
+//main fuction
+int main(){
+  Node* root=create(1);
+  root->left=create(2);
+  root->left->right=create(5);
+  root->left->left=create(4);
+  root->right=create(3);
+  root->right->left=create(6);
+  root->right->right=create(7);
+    cout<<"level of 5 is :"<<" ";
+    cout<<getLevel(root,5);
+    return 0;
 }
-
-// This code is contributed by @tanmaysharma17
