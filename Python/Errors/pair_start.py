7a5f39835d60141a2cf5d6b33915adb6cc67fe76
Yash Mohan <41343709+yashmohan100@users.noreply.github.com>
2021-10-28 09:15:38 +0530

    Update pair_start.py

--- Python/Errors/pair_start.py
+++ Python/Errors/pair_start.py
@@ -5,16 +5,19 @@
 # each other by a "*".
 # Read input as specified in the question.
 # Print output as specified in the question.
-def pair(s):
-    ch = ''
-    small = pair(s[2:])
-    if s[1] == s[2]:
-        ch = ch+'*'
-        return ch+small
-    else:
-        ch = ch+'*'
-        return ch+small
 
-
+def pairStar(Input, Output, i = 0) :
+     
+    Output = Output + Input[i]
+    if (i == len(Input) - 1) :
+        print(Output)
+        return;
+    
+    if (Input[i] == Input[i + 1]) :
+        Output = Output + '*';
+ 
+    pairStar(Input, Output, i + 1);
+    
 s = input()
-print(pair(s))
+output = ""
+pairStar(s, output) 
