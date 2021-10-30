2db98da722bbaa84d3a5f01ab0c1166b97274074
NivedithaReddyM <nivedithareddym27@gmail.com>
2021-10-30 21:15:18 +0530

    Go through the code

--- Python/Tasks/number_of_letters_and_digits.py
+++ Python/Tasks/number_of_letters_and_digits.py
@@ -6,11 +6,19 @@ Then, the output should be:
 LETTERS 10
 DIGITS 3
 """
+letters=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+digits=['1','2','3','4','5','6','7','8','9','0']
+l=0
+d=0
+s=input("Enter the string:")
 
-s = raw_input("enter the sentence")
-d={"DIGITS":0, "LETTERS":0}
-for c in s:
-   
-        pass
-print "LETTERS", d["LETTERS"]
-print "DIGITS", d["DIGITS"]
+for i in s:
+        for j in letters:
+                if(i.upper()==j):
+                        l+=1
+        for k in digits:
+                if(i==k):
+                        d+=1
+print("No.of letters=",l)
+print("No.of digits=",d)
+                
--- Python/Tasks/number_of_upper_lower_case_letter.py
+++ Python/Tasks/number_of_upper_lower_case_letter.py
@@ -1,16 +1,13 @@
 # Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 
-def count_caps():
+def calculate():
     s = input("Please enter the string...")
     upper = 0
     lower = 0
-    length = len(s)
-    i = 0
-    while i < length:
-        if s[i].isupper():
-            upper += 1
-        if s[i].islower():
-            lower += 1
-        i += 1
+    for i in s:
+        if(i.isupper()):
+            upper+=1
+        if(i.islower()):
+            lower+=1
     print("String has " + str(upper) + " uppercase and " + str(lower) + " lowercase characters.")
-count_caps()
+calculate()
